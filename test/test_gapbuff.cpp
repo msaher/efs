65603b1603226342485ab2527c7da23fe4fa827e
saher <msaher.shair@gmail.com>
2022-07-29 11:45:02 +0300

    add unit tests

---
+++ test/test_gapbuff.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE gapbuff
+#include <boost/test/unit_test.hpp>
+#include "../src/gapbuff.h"
+#include <string>
+
+using std::string;
+
+BOOST_AUTO_TEST_CASE(gapbuff_test)
+{
+    BOOST_CHECK(1 + 1 == 2);
+
+    GapBuff<char> buf{};
+    buf.insert('c');
+    buf.insert('d');
+    std::cout << to_string(buf);
+    BOOST_CHECK(buf.left() == true);
+    std::cout << to_string(buf);
+}
