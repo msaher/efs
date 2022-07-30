c71c52652b26aaf1791e2fe49281c24e27913394
saher <msaher.shair@gmail.com>
2022-07-30 10:03:03 +0300

    got rid of accidental duplicate code

--- src/editor.h
+++ src/editor.h
@@ -1,13 +1,15 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 #include "gapbuff.h"
+#include <vector>
+
+using std::vector;
 
 struct Editor {
-    unsigned int cx; // x coordinate
+    unsigned int cx; // actual x coordinate
     unsigned int cy; // y coordinate
     unsigned int screen_cols; // window size
     unsigned int screen_rows;
-    GapBuff<char> buf; // 2d gap buffer
+    vector<GapBuff<char>> buf;
 };
-
 #endif
