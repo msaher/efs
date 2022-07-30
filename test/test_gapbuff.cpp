539de9be0b8297ac8c4d78ce8f6b67a1114da191
saher <msaher.shair@gmail.com>
2022-07-30 11:08:32 +0300

    add more tests

--- test/test_gapbuff.cpp
+++ test/test_gapbuff.cpp
@@ -6,12 +6,24 @@
 
 using std::string;
 
-BOOST_AUTO_TEST_CASE(gapbuff_test)
+BOOST_AUTO_TEST_CASE(leftright)
 {
-    BOOST_CHECK(1 + 1 == 2);
-
-    GapBuff<char> buf{};
+    GapBuff<char> buf{10};
+    buf.insert('a');
+    buf.insert('b');
     buf.insert('c');
     buf.insert('d');
+    buf.insert('e');
+    buf.insert('f');
+    buf.insert('g');
     BOOST_CHECK(buf.left() == true);
+    BOOST_CHECK(buf.right() == true);
+    BOOST_CHECK(buf.right() == false);
+}
+
+BOOST_AUTO_TEST_CASE(strconst)
+{
+    string s = "this is a piece of text";
+    GapBuff<char> buf {s};
+    BOOST_CHECK(buf.to_string() == "_" + s);
 }
