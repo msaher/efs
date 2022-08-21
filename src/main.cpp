ca2ca7c14514c79827165962a3de42f4f0d72d85
saher <msaher.shair@gmail.com>
2022-08-21 23:20:15 +0300

    Handle cli arguments

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,42 @@
 #include "editor.h"
 #include <fstream>
 #include "keys.h"
+#include <iostream>
+#include <unistd.h>
 
 using std::string;
+using std::cerr;
 using std::ifstream;
 
 Editor ED {}; // global editor state
 
-int main(int, char* argv[])
+void handle_args(int argc, char* argv[])
 {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " filename" << "\n";
+        exit(1);
+    }
+
+    if (access(argv[1], R_OK) != 0) {
+        cerr << "Can't read file " << argv[1] << "\n";
+        exit(1);
+    }
+    else {
+        ED.filename = argv[1];
+        load(ED, ED.filename);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    handle_args(argc, argv);
+
     set_raw();
     atexit(cleanup_term);
 
     window_size(ED.screen_rows, ED.screen_cols);
     ED.screen_rows -= 2; // room for bar and messages
     ED.mode = NORMAL;
-
-    if (*++argv != nullptr) {
-        ED.filename = *argv;
-        load(ED, ED.filename);
-    }
 
     int c;
     while (true) {
